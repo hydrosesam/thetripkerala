573ff9aa9db1d2da1cec9c60707bd6dd6d24ac92
hydrosesam <71249432+hydrosesam@users.noreply.github.com>
2024-01-17 22:12:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 // 'auto' | 'force-dynamic' | 'error' | 'force-static'
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
